02056584d8d0b85e77c4009090d7b9db1cbd9bda
unknown <2302520@ub.edu.ph>
2025-09-20 14:58:06 +0800

    React Render HTML Done all examples

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//The createRoot Function
+//import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//createRoot(document.getElementById('root')).render(
+  //<h1>Hello React!</h1>
+  
+//)
+
+//import { createRoot } from 'react-dom/client'
+
+//Show React
+//createRoot(document.getElementById('root')).render(
+  //<p>Welcome!</p>
+  
+//)
+
+//The HTML Code
+//import { createRoot } from 'react-dom/client'
+
+//const myelement = (
+  //<table>
+    //<tr>
+      //<th>Name</th>
+    //</tr>
+    //<tr>
+      //<td>John</td>
+    //</tr>
+    //<tr>
+      //<td>Elsa</td>
+    //</tr>
+  //</table>
+//);
+
+//createRoot(document.getElementById('root')).render (
+  //myelement
+//)
+
+import {createRoot} from 'react-dom/client'
+
+createRoot(document.getElementById('sandy')).render(
+  <p>Welcome!</p>
 )
